15a5b6f18f8c883d04dfe576866eae9818dae064
Nina Hahne <ninamariehahne@gmail.com>
2020-02-07 17:08:31 +0100

    part 9 - initial reducers setup

--- src/reducers.js
+++ src/reducers.js
@@ -1,26 +1,41 @@
 
 export default function reducer(state = {}, action) {
-    if (action.type === 'ALL_CAPS_WITH_UNDERSCORES') {
+    if (action.type === 'RECEIVE_FRIENDS_WANNABES') {
         // cloning global redux state with spread operator:
         state = {
             ... state,
-            friendsWannabes: []
+            friendsWannabes: action.friendsWannabes
         };
-        // ***immutable methods for making changes to redux:***
-        // map - good for changing item(s) in an array
-        // filter - removes item(s) from an array
-        // concat - combine two or more arrays into one array
-        //  ... (spread operator) - copy arrays and objects and add properties to those copies
-        // OR (instead of ...): Object.assign  - make copies of objects
     }
 
-    // if (true) {
+    if (action.type === 'ACCEPT_FRIEND_REQUEST') {
+
+        state = {
+            ... state,
+            acceptedUserId: action.acceptedUserId
+        };
+    }
+
+    if (action.type === 'UNFRIEND') {
+
+        state = {
+            ... state,
+            unfriendedUserId: action.unfriendedUserId
+        };
+    }
+
+    // if (action.type === 'ALL_CAPS_WITH_UNDERSCORES') {
     //
     // }
-    //
-    // if (true) {
-    //
-    // }
+
 
     return state;
 }
+
+
+// ***immutable methods for making changes to redux:***
+// map - good for changing item(s) in an array
+// filter - removes item(s) from an array
+// concat - combine two or more arrays into one array
+//  ... (spread operator) - copy arrays and objects and add properties to those copies
+// OR (instead of ...): Object.assign  - make copies of objects
